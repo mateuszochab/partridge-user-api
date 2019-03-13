373585bcf3fed6fc70320b1d0b721dcb0cc8b51b
Mateusz Ochab <ochab.mat@gmail.com>
2019-03-13 11:02:25 +0100

    s

--- src/test/java/com/ochabmateusz/partridge/partridgeuserapi/business/UserServiceBusinessTest.java
+++ src/test/java/com/ochabmateusz/partridge/partridgeuserapi/business/UserServiceBusinessTest.java
@@ -27,93 +27,67 @@ public class UserServiceBusinessTest {
     @Test
     public void getAllUsers() {
 
-        when(mongoRepo.findAll()).thenReturn(Arrays.asList(new User(new UserCore("Iguana1","Ziomek12345","Iguana123456","igga@gmail.com")),
-                new User(new UserCore("Iguana1","Ziomek12345","Iguana123456","igga@gmail.com"))));
+        when(mongoRepo.findAll()).thenReturn(Arrays.asList(new User(new UserCore("Iguana1", "Ziomek12345", "Iguana123456", "igga@gmail.com")),
+                new User(new UserCore("Iguana1", "Ziomek12345", "Iguana123456", "igga@gmail.com"))));
 
-        assertEquals(2,userServiceBusiness.getAllUsers().size());
+        assertEquals(2, userServiceBusiness.getAllUsers().size());
 
     }
 
     @Test
     public void getUserByNickName() {
-        when(mongoRepo.findByNickNameQuery("Iguana123456")).thenReturn(new User(new UserCore("Iguana1","Ziomek12345","Iguana123456","igga@gmail.com")));
+        when(mongoRepo.findByNickNameQuery("Iguana123456")).thenReturn(new User(new UserCore("Iguana1", "Ziomek12345", "Iguana123456", "igga@gmail.com")));
 
 
-        assertEquals("Iguana123456",userServiceBusiness.getUserByNickName("Iguana123456").getUserCore().getNickname());
+        assertEquals("Iguana123456", userServiceBusiness.getUserByNickName("Iguana123456").getUserCore().getNickname());
 
     }
 
     @Test
     public void getPassword() {
-        when(mongoRepo.findByNickNameQuery("Iguana123456")).thenReturn(new User(new UserCore("Iguana1","Ziomek12345","Iguana123456","igga@gmail.com")));
+        when(mongoRepo.findByNickNameQuery("Iguana123456")).thenReturn(new User(new UserCore("Iguana1", "Ziomek12345", "Iguana123456", "igga@gmail.com")));
 
 
-        assertEquals("Ziomek12345",userServiceBusiness.getUserByNickName("Iguana123456").getUserCore().getPassword());
+        assertEquals("Ziomek12345", userServiceBusiness.getUserByNickName("Iguana123456").getUserCore().getPassword());
     }
 
     @Test
     public void getEmail() {
-        when(mongoRepo.findByNickNameQuery("Iguana123456")).thenReturn(new User(new UserCore("Iguana1","Ziomek12345","Iguana123456","igga@gmail.com")));
+        when(mongoRepo.findByNickNameQuery("Iguana123456")).thenReturn(new User(new UserCore("Iguana1", "Ziomek12345", "Iguana123456", "igga@gmail.com")));
 
 
-        assertEquals("igga@gmail.com",userServiceBusiness.getUserByNickName("Iguana123456").getUserCore().getEmail());
+        assertEquals("igga@gmail.com", userServiceBusiness.getUserByNickName("Iguana123456").getUserCore().getEmail());
     }
 
     @Test
     public void getName() {
-        when(mongoRepo.findByNickNameQuery("Iguana123456")).thenReturn(new User(new UserCore("Iguana1","Ziomek12345","Iguana123456","igga@gmail.com")));
+        when(mongoRepo.findByNickNameQuery("Iguana123456")).thenReturn(new User(new UserCore("Iguana1", "Ziomek12345", "Iguana123456", "igga@gmail.com")));
 
 
-        assertEquals("Iguana1",userServiceBusiness.getUserByNickName("Iguana123456").getUserCore().getName());
+        assertEquals("Iguana1", userServiceBusiness.getUserByNickName("Iguana123456").getUserCore().getName());
     }
 
 
     @Test
     public void getAllNotConfirmed() {
-        when(mongoRepo.findAllNotConfirmed(false)).thenReturn(Arrays.asList(new User(new UserCore("Iguana1","Ziomek12345","Iguana123456","igga@gmail.com")),
-                new User(new UserCore("Iguana1","Ziomek12345","Iguana123456","igga@gmail.com")),
-                new User(new UserCore("Iguana1","Ziomek12345","Iguana123456","igga@gmail.com"))));
+        when(mongoRepo.findAllNotConfirmed(false)).thenReturn(Arrays.asList(new User(new UserCore("Iguana1", "Ziomek12345", "Iguana123456", "igga@gmail.com")),
+                new User(new UserCore("Iguana1", "Ziomek12345", "Iguana123456", "igga@gmail.com")),
+                new User(new UserCore("Iguana1", "Ziomek12345", "Iguana123456", "igga@gmail.com"))));
 
-        assertEquals(3,userServiceBusiness.getAllNotConfirmed().size());
+        assertEquals(3, userServiceBusiness.getAllNotConfirmed().size());
 
 
     }
 
     @Test
     public void getAllModified() {
-        when(mongoRepo.findAllModified()).thenReturn(Arrays.asList(new User(new UserCore("Iguana1","Ziomek12345","Iguana123456","igga@gmail.com")),
-                new User(new UserCore("Iguana1","Ziomek12345","Iguana123456","igga@gmail.com")),
-                new User(new UserCore("Iguana1","Ziomek12345","Iguana123456","igga@gmail.com"))));
+        when(mongoRepo.findAllModified()).thenReturn(Arrays.asList(new User(new UserCore("Iguana1", "Ziomek12345", "Iguana123456", "igga@gmail.com")),
+                new User(new UserCore("Iguana1", "Ziomek12345", "Iguana123456", "igga@gmail.com")),
+                new User(new UserCore("Iguana1", "Ziomek12345", "Iguana123456", "igga@gmail.com"))));
 
 
         assertEquals(3, userServiceBusiness.getAllModified().size());
 
     }
 
-    @Test
-    public void confirmUser() {
-//
-//        when(mongoRepo.findByNickNameQuery("Iguana123456")).thenReturn(new User(new UserCore("Iguana1","Ziomek12345","Iguana123456","igga@gmail.com")));
-//        when(mongoRepo.save(mongoRepo.findByNickNameQuery("Iguana123456") )).thenReturn()
-//        userServiceBusiness.confirmUser("Iguana123456");
-//
-//        assertTrue();
-
-    }
-
-    @Test
-    public void getAllByCreatedDate() {
-    }
-
-    @Test
-    public void getRole() {
-    }
-
-    @Test
-    public void setAdmin() {
-    }
-
-    @Test
-    public void setPaid() {
-    }
 }
